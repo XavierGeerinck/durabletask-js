41aa62d382cf4774b1a4a52655da0823d2594489
Xavier Geerinck <xavier.geerinck@gmail.com>
2023-08-04 18:53:01 -0700

    ensure test fails if script fails
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -10,5 +10,11 @@ docker run \
 echo "Running E2E tests"
 npm run test test/e2e
 
+# It should fail if the npm run fails
+if [ $? -ne 0 ]; then
+    echo "E2E tests failed"
+    exit 1
+fi
+
 echo "Stopping Sidecar"
 docker stop durabletask-sidecar
